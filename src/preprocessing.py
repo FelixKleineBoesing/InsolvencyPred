3ad360e4966e44ae67642e4df038b18ee9df55f6
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-10-18 21:03:02 +0200

    addedd PCA and a naive CorrelationRemover

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,15 +1,18 @@
 import abc
 import numpy as np
 import pandas as pd
+from typing import Union
+from sklearn.decomposition import PCA as PCASK
 
 
-def _sampling(data, method: str = "up"):
+def _sampling(data, label, method: str = "up"):
     """
 
     :param data:
     :return:
     """
     assert method in ["up", "down"]
+    data["class"] = label
     if method == "up":
         factor = -1
     else:
@@ -30,7 +33,11 @@ def _sampling(data, method: str = "up"):
             args["replace"] = True
         down_sampled_df = class_neg.sample(count_pos, replace=True)
         sampled_data = pd.concat((down_sampled_df, class_pos))
-    return sampled_data
+    sampled_data.reset_index(inplace=True, drop=True)
+    sampled_data = sampled_data.iloc[np.random.permutation(sampled_data.shape[0]),:]
+    sampled_data.reset_index(inplace=True, drop=True)
+    sampled_label = sampled_data.pop("class")
+    return sampled_data, sampled_label
 
 
 def remove_outliers(values, iqr_factor: float = 2.5):
@@ -70,16 +77,54 @@ def _get_iqr(values):
 class Preprocessor(abc.ABC):
 
     @abc.abstractmethod
-    def process(self, train_data, test_data=None, train_label=None) -> (pd.DataFrame, pd.DataFrame):
+    def process(self, train_data, test_data=None, train_label=None) -> (pd.DataFrame, pd.DataFrame,
+                                                                        Union[pd.Series, list]):
         """
         This method may mutate the train and test data and return them afterwards
 
         :param train_data:
         :param test_data:
         :param train_label:
-        :return: a tuple that contains the train and test dataframes
+        :return: a tuple that contains the train and test dataframes and the label
         """
         pass
+
+
+class PCA(Preprocessor):
+
+    def __init__(self, variance_threshold: float = 0.95):
+        self.variance_threshold = variance_threshold
+
+    def process(self, train_data, test_data=None, train_label=None) -> (pd.DataFrame, pd.DataFrame,
+                                                                        Union[pd.Series, list]):
+        pca = PCASK(n_components=self.variance_threshold)
+        pca.fit(train_data)
+        train_data = pd.DataFrame(pca.transform(train_data))
+        test_data = pd.DataFrame(pca.transform(test_data))
+
+        return train_data, test_data, train_label
+
+
+class CorrelationRemover(Preprocessor):
+
+    def __init__(self, number_dropped_features: int = 10, squared: bool = False):
+        self.number_dropped_features = number_dropped_features
+        self.squared = squared
+
+    def process(self, train_data, test_data=None, train_label=None) -> (pd.DataFrame, pd.DataFrame,
+                                                                        Union[pd.Series, list]):
+        assert train_data.shape[1] > self.number_dropped_features
+
+        for i in range(self.number_dropped_features):
+            corrs = train_data.corr()
+            if self.squared:
+                corrs = np.sqrt(corrs)
+            sum_corrs = corrs.sum(axis=1)
+            col_to_remove = corrs.index[np.nanargmax(np.abs(sum_corrs))]
+            train_data = train_data.drop(col_to_remove, axis=1)
+            if test_data is not None:
+                test_data = test_data.drop(col_to_remove, axis=1)
+        return train_data, test_data, train_label
 
 
 class ReSampler(Preprocessor):
@@ -88,7 +133,8 @@ class ReSampler(Preprocessor):
         self.method = method
 
     def process(self, train_data, test_data=None, train_label=None) -> (pd.DataFrame, pd.DataFrame):
-        return _sampling(train_data, self.method), test_data
+        data, label = _sampling(train_data, train_label, self.method)
+        return data, test_data, label
 
 
 class MeanReplacement(Preprocessor):
@@ -99,7 +145,7 @@ class MeanReplacement(Preprocessor):
             train_data.loc[~np.isfinite(train_data[col]), col] = mean_train
             if test_data is not None:
                 test_data.loc[~np.isfinite(test_data[col]), col] = mean_train
-        return train_data, test_data
+        return train_data, test_data, train_label
 
 
 class Standardizer(Preprocessor):
@@ -111,7 +157,7 @@ class Standardizer(Preprocessor):
         train_data = (train_data - mean_train) / sd_train
         if test_data is not None:
             test_data = (test_data - mean_train) / sd_train
-        return train_data, test_data
+        return train_data, test_data, train_label
 
 
 if __name__ == "__main__":
@@ -120,16 +166,12 @@ if __name__ == "__main__":
     clipped_values = remove_outliers(values, 1.5)
     print(min(clipped_values))
     print(max(clipped_values))
-
-    data = pd.DataFrame({"class": [1, 1, 0, 1], "a": [1, 2, 3, 4]})
-    upsampled_data = up_sampling(data)
-    downsampled_data = down_sampling(data)
-
+    data = pd.DataFrame({"class": [1, 1, 0, 1, 0,0], "a": [1, 2, 3, 4, 5, 6]})
     data_na = pd.DataFrame({"class": [1, 1, 0, 1, 0,0], "a": [1, 2, 3, 4, np.NaN, np.Inf]})
     mean_replacer = MeanReplacement()
     mean_replacer.process(data_na)
 
-    standardizer = Standardizer()
-    standardizer.process(data)
+    correlation = CorrelationRemover(1)
+    correlation.process(data_na)
 
 
