fix recall and precision

src/__main__.py
@@ -3,7 +3,7 @@ import numpy as np
 from cleaning import load_arff_files
 from modelling import LogisticRegression, XGBoost, CrossValidation
 from preprocessing import Standardizer, MeanReplacement, clip_outliers, ReSampler, CorrelationRemover, PCA
-from evaluation import  get_f1_score, get_accuracy, get_auc
+from evaluation import  get_f1_score, get_accuracy, get_auc, get_precision, get_recall
 from visualization import get_roc_curve
 
 
@@ -22,7 +22,7 @@ def main():
         df = clip_outliers(df.copy(), 5.5)
         model = LogisticRegression(max_iter=1000)
         probs = cv.run(data=df, label=label, model=model,
-                       preprocessors=[MeanReplacement(), Standardizer(), ReSampler("down")])
+                       preprocessors=[MeanReplacement(), Standardizer(), ReSampler("down"), PCA(0.98)])
         probs_lr.append(probs) #
         preds = [1 if p > 0.5 else 0 for p in probs_lr[i]]
         print("F1, Baseline: {}, LogisticRegression: {}".format(get_f1_score(np.random.choice([0, 1], size=len(label)), label),
@@ -31,6 +31,10 @@ def main():
                                                                  get_accuracy(preds, label)))
         print("AUC, Baseline: {}, LogisticRegression: {}".format(get_auc(np.random.choice([0, 1], size=len(label)), label),
                                                                  get_auc(probs_lr[i], label)))
+        print("Recall, Baseline: {}, LogisticRegression: {}".format(get_recall(np.random.choice([0, 1], size=len(label)), label),
+                                                         get_recall(preds, label)))
+        print("Precision, Baseline: {}, LogisticRegression: {}".format(get_precision(np.random.choice([0, 1], size=len(label)), label),
+                                                            get_precision(preds, label)))
     get_roc_curve(probs_lr[i], label)
 
     for i, df in enumerate(dfs):
@@ -39,7 +43,7 @@ def main():
         label = df.pop("class")
         model = XGBoost(val_share=0.2, n_rounds=10, lambda_=0,
                         additional_booster_params={"params": {"max_depth": 4, "subsample": 0.7,
-                                                              "colsample_bytree": 0.7}})
+                                                              "colsample_bytree": 0.7, "scale_pos_weight": 13}})
         probs_xgb.append(cv.run(data=df, label=label, model=model, preprocessors=[ReSampler("down")]))
 
         preds = [1 if p > 0.5 else 0 for p in probs_xgb[i]]
@@ -47,8 +51,12 @@ def main():
                                                      get_f1_score(preds, label)))
         print("Acc, Baseline: {}, Xgboost: {}".format(get_accuracy([0 for _ in range(len(label))], label),
                                                       get_accuracy(preds, label)))
-        print("AUC, Baseline: {}, Xgboost: {}".format(get_auc(np.random.choice([0, 1], size=len(label)), label),
+        print("AUC, Baseline: {}, Xgboost: {}".format(get_auc(np.random.rand(len(label)), label),
                                                       get_auc(probs_xgb[i], label)))
+        print("Recall, Baseline: {}, Xgboost: {}".format(get_recall(np.random.choice([0, 1], size=len(label)), label),
+                                                         get_recall(preds, label)))
+        print("Precision, Baseline: {}, Xgboost: {}".format(get_precision(np.random.choice([0, 1], size=len(label)), label),
+                                                            get_precision(preds, label)))
     get_roc_curve(probs_xgb[i], label)
 
 

src/evaluation.py
@@ -30,7 +30,7 @@ def get_recall(preds, actuals):
     preds = np.array(preds)
     actuals = np.array(actuals)
     dividend = (np.sum(np.logical_and(preds == 1, actuals == 1)) +
-                np.sum(np.logical_and(preds == 1, actuals == 0)))
+                np.sum(np.logical_and(preds == 0, actuals == 1)))
     if dividend == 0:
         return 0
     else:
@@ -41,7 +41,7 @@ def get_precision(preds, actuals):
     preds = np.array(preds)
     actuals = np.array(actuals)
     return np.sum(np.logical_and(preds == 1, actuals == 1)) / (np.sum(np.logical_and(preds == 1, actuals == 1)) +
-                                                               np.sum(np.logical_and(preds == 0, actuals == 1)))
+                                                               np.sum(np.logical_and(preds == 1, actuals == 0)))
 
 
 class EvaluationTracker: