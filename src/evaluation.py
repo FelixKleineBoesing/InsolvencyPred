ce3c179032bdf9304a1f4bca577c7e00e32ff8e3
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-10-18 21:04:42 +0200

    fix evaluation functions

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,31 +1,42 @@
 import numpy as np
+from sklearn.metrics import roc_auc_score
 
 
 def get_auc(probs, actuals):
-    # TODO
-    pass
-
-
-def get_roc_curve(probs, actuals):
-    # TODO
-    pass
+    return roc_auc_score(actuals, probs)
 
 
 def get_f1_score(preds, actuals):
+    preds = np.array(preds)
+    actuals = np.array(actuals)
     precision = get_precision(preds, actuals)
     recall = get_recall(preds, actuals)
-    return (2 * precision * recall) / (precision + recall)
+    dividend = (precision + recall)
+    if dividend == 0:
+        return 0
+    else:
+        return (2 * precision * recall) / (precision + recall)
 
 
 def get_accuracy(preds, actuals):
-    return (preds == actuals) / len(preds)
+    preds = np.array(preds)
+    actuals = np.array(actuals)
+    return np.sum(preds == actuals) / len(preds)
 
 
 def get_recall(preds, actuals):
-    return np.sum(np.logical_and(preds == 1, actuals == 1)) / (np.sum(np.logical_and(preds == 1, actuals == 1)) +
-                                                               np.sum(np.logical_and(preds == 1, actuals == 0)))
+    preds = np.array(preds)
+    actuals = np.array(actuals)
+    dividend = (np.sum(np.logical_and(preds == 1, actuals == 1)) +
+                np.sum(np.logical_and(preds == 1, actuals == 0)))
+    if dividend == 0:
+        return 0
+    else:
+        return np.sum(np.logical_and(preds == 1, actuals == 1)) / dividend
 
 
 def get_precision(preds, actuals):
+    preds = np.array(preds)
+    actuals = np.array(actuals)
     return np.sum(np.logical_and(preds == 1, actuals == 1)) / (np.sum(np.logical_and(preds == 1, actuals == 1)) +
                                                                np.sum(np.logical_and(preds == 0, actuals == 1)))
